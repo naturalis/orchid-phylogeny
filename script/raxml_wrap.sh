61d872955b7cdc961399b253e56196fa4c3c5bfc
Rutger Vos <rutgeraldo@gmail.com>
2019-10-30 17:37:08 +0100

    modify me for sbatch

--- script/raxml_wrap.sh
+++ script/raxml_wrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DIR=$1
 TREE=$2
-PARTITIONS=$3
+PARTITIONS=$3 
 INFILES=$(ls "$DIR"/*.phy.gz)
 for INFILE in $INFILES; do
-  raxml_wrap.pl -m "$INFILE" -t "$TREE" -p "$PARTITIONS"
+  raxml_wrap.pl -m "$INFILE" -t "$TREE" -p "$PARTITIONS" -c 2
   rm $PARTITIONS.reduced
 done

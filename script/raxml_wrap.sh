4a5067feefebff7a3eb1faab38c099770c66a54c
Rutger Vos <rutgeraldo@gmail.com>
2019-11-04 10:34:32 +0100

    Update raxml_wrap.sh

--- script/raxml_wrap.sh
+++ script/raxml_wrap.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-DIR=$1
-TREE=$2
-PARTITIONS=$3 
+#SBATCH --job-name=raxml-orchid
+#SBATCH --output=raxml-orchid.log
+DIR=/home/luis.valente/orchid-phylogeny-data/bootstrapped
+TREE=/home/luis.valente/orchid-phylogeny/data/Orchid_OTT.tre
+PARTITIONS=/home/luis.valente/orchid-phylogeny/data/raxml_partitions.txt
 INFILES=$(ls "$DIR"/*.phy.gz)
+RAXML=raxmlHPC
 for INFILE in $INFILES; do
-  raxml_wrap.pl -m "$INFILE" -t "$TREE" -p "$PARTITIONS" -c 2
-  rm $PARTITIONS.reduced
+	perl /home/luis.valente/orchid-phylogeny/script/raxml_wrap.pl -m "$INFILE" -t "$TREE" -p "$PARTITIONS" -c 16 -r $RAXML
+	if [ -f $PARTITIONS.reduced ]; then
+		rm $PARTITIONS.reduced
+	fi
 done

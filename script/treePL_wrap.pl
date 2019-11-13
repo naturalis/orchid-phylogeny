bf683ee78fb824e802758c3f8e6c26ccfc026844
Rutger Vos <rutgeraldo@gmail.com>
2019-11-13 22:12:54 +0100

    Update treePL_wrap.pl

--- script/treePL_wrap.pl
+++ script/treePL_wrap.pl
@@ -13,20 +13,20 @@ GetOptions(
 opendir my $dh, $indir or die $!;
 while( my $entry = readdir $dh ) {
 	if ( $entry =~ /^RAxML_result\.rep.*dnd$/ ) {
-		my $outfile = $indir . '/' . $entry . '.out';
-		my $logfile = $indir . '/' . $entry . '.log';
-		my $cmdfile = $indir . '/' . $entry . '.cmd';
-		my $intree  = $indir . '/' . $entry;
+		my $outfile = $entry . '.out';
+		my $logfile = $entry . '.log';
+		my $cmdfile = $entry . '.cmd';
 		
 		open my $in, '<', $template or die $!;
 		open my $out, '>', $cmdfile or die $!;
 		while(<$in>) {
-			s/Orchid\.tre/$intree/;
+			s/Orchid\.tre/$entry/;
 			s/Orchid_dated\.tre/$outfile/;
 			print $out $_;
 		}
 		close $in;
 		close $out;
+		
 		system("sudo treePL $cmdfile > $logfile");
 	}
 }	
